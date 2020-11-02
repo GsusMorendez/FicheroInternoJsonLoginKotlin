a4a9b2440476b8aab1609a2f1e494bd02fe7d7b5
Jesus <jesusmorendez@gmail.com>
2020-11-02 21:36:33 +0100

    ajustes2

--- app/src/main/java/com/example/morenohernandezjesusjsonfichero/MainActivity.kt
+++ app/src/main/java/com/example/morenohernandezjesusjsonfichero/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.morenohernandezjesusjsonfichero
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.morenohernandezjesusjsonfichero.activities.RegistroActivity
 import com.example.morenohernandezjesusjsonfichero.dialogs.Login
@@ -21,8 +23,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonLogin: Button
     lateinit var buttonInformacion: Button
     lateinit var miUsuarioLogeado: Usuario
-    lateinit var syso: EditText
     var alguienLogueado = -1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +33,12 @@ class MainActivity : AppCompatActivity() {
         buttonLogin = findViewById<Button>(R.id.buttonLogin)
         buttonInformacion = findViewById<Button>(R.id.buttonInformacion)
 
-        buttonLogin.isEnabled = comprobarActivacionLogin()
+        try {
+            buttonLogin.isEnabled = comprobarActivacionLogin()
+        } catch (e: Exception) {
+            var fileOutput = openFileOutput("ficheroInternoUsuarios.txt", Context.MODE_PRIVATE)
+        }
+
         alguienLogueado = intent.getIntExtra("alguienLogeado", 0)
         var vengoDe = intent.getStringExtra("vengoDe")
 
